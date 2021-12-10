80d7dc0b13a83ce5fcbc210cf3f3a1b4f48e1b54
nina.marie <nina.kaunisvaara@edu.keuda.fi>
2021-12-10 23:42:19 +0200

    10.12 tallennus, kesken

--- projektit/projekti1b.js
+++ projektit/projekti1b.js
@@ -1,42 +1,79 @@
 function kysely() {
 
+  var kylla = document.getElementById('kylla').checked;
+  var ei = document.getElementById('ei').checked;
+  var entiia = document.getElementById('enTieda').checked;
+
+  if(kylla == false && ei == false && entiia == false) {
+    alert('Valitse vaihtoehto kohdasta "Suosittelisitko opintojasi muille"');
+    return false;
+  }
+  else{
+    return true;
+  }
+
+  var valinnat = document.forms["Kyselylomake"]["valinnat"];
+
+  if (form.valinnat[0].selected == true) {
+    window.alert("Ole hyvä ja valitse toimenkuva");
+    return false;
+  }
+
+  if (viesti.value == "") {
+   window.alert("Viestikenttä on tyhjä.");
+   return false;
+  }
+
+  var yx = document.getElementById('boxyx').checked;
+  var kax = document.getElementById('boxkax').checked;
+  var kol = document.getElementById('boxkol').checked;
+
+  if(yx == false && kax == false && kol == false) {
+    alert('Valitse vaihtoehto kohdasta "Parasta koulussa on"');
+    return false;
+  }
+  else{
+    return true;
+  }
+
+  window.addEventListener("DOMContentLoaded", function(){
+    var nimi = document.getElementById("nimi");
+    nimi.addEventListener("input", function(){
+      if (nimi.validity.tooShort || nimi.validity.valueMissing) {
+        nimi.setCustomValidity("Nimessä on oltava vähintään viisi kirjainta");
+        nimi.reportValidity();
+      }
+      else {
+        nimi.setCustomValidity("");
+      }
+    });
+  });
+
+
+   window.addEventListener("DOMContentLoaded", function(){
+   var ika = document.getElementById("ika");
+   ika.addEventListener("input", function(){
+     if (ika.validity.invalid || ika.validity.valueMissing) {
+       ika.setCustomValidity("Syötä ikä numeroina");
+       ika.reportValidity();
+     }
+     else if (ika.validity.tooLong) {
+       ika.setCustomValidity("Iän on oltava luku väliltä 1-99");
+       ika.reportValidity();
+     }
+     else {
+       ika.setCustomValidity("");
+     }
+   });
+  });
+
 }
 
 
 function emailIsValid (email) {
    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
 
-   if(emailIsValid(sahkoposti)) {
+   if(emailIsValid(email)) {
     }
-    else{ alert("Anna oikea sÃ¤hkÃ¶postiosoitteesi"); lomake.email.focus(); return (false); }
+    else{ alert("Anna oikea sähköpostiosoitteesi"); Kyselylomake.email.focus(); return (false); }
  }
-
-window.addEventListener("DOMContentLoaded", function(){
-  var nimi = document.getElementById("nimi");
-  nimi.addEventListener("input", function(){
-    if (nimi.validity.tooShort || nimi.validity.valueMissing) {
-      nimi.setCustomValidity("Nimessä on oltava vähintään viisi kirjainta");
-      nimi.reportValidity();
-    }
-    else {
-      nimi.setCustomValidity("");
-    }
-  });
-});
-
- window.addEventListener("DOMContentLoaded", function(){
- var ika = document.getElementById("ika");
- ika.addEventListener("input", function(){
-   if (ika.validity.invalid || ika.validity.valueMissing) {
-     ika.setCustomValidity("Syötä ikä numeroina");
-     ika.reportValidity();
-   }
-   else if (ika.validity.tooLong) {
-     ika.setCustomValidity("Iän on oltava luku väliltä 1-99");
-     ika.reportValidity();
-   }
-   else {
-     ika.setCustomValidity("");
-   }
- });
-});
