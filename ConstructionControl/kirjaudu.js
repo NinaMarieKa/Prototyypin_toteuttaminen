593611c9611112f75dcac02c04c953b13acae4bf
nina.marie <nina.kaunisvaara@edu.keuda.fi>
2022-04-16 20:47:20 +0300

    16.4 log in

--- ConstructionControl/kirjaudu.js
+++ ConstructionControl/kirjaudu.js
@@ -10,15 +10,41 @@ images[4] ="kuvat/kuva5.jpg";
 
 function changeImg() {
   document.slides.src = images[i];
-
   if (i < images.length -1) {
     i++
   }
   else {
     i = 0;
   }
-
   setTimeout("changeImg()", time)
 }
 
 window.onload = changeImg;
+
+function KirjauduSisaanTyo() {
+
+  var tunnusTyo = document.getElementById('tunnusTyo').value;
+  var salisTyo = document.getElementById('salasana').value;
+
+  if(tunnusTyo == "esimerkki" && salisTyo == "esimerkki")
+  {
+    alert("Kirjautuminen onnistui. Sinut ohjataan lomakesivulle.")
+    window.location.href="lomake_tyontekijat.html";
+  }
+  else {
+    alert("Väärä tunnus tai salasana.");
+  }
+}
+function KirjauduSisaanHallinta() {
+
+  var tunnusHallinta = document.getElementById('tunnusHallinta').value;
+  var salisHallinta = document.getElementById('sisaanHallinta').value;
+
+  if (tunnusHallinta == "esimerkki" && salisHallinta == "esimerkki") {
+    alert("Kirjautuminen onnistui. Sinut ohjataan aloitussivulle.")
+    window.location.href="aloitus_hallinta.html";
+  }
+  else {
+    alert("Väärä tunnus tai salasana.");
+  }
+}
