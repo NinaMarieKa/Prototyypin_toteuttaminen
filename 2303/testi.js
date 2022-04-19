c2a776d49cd2f4d38cbc7f357ddb1ebc7c0be7b6
nina.marie <nina.kaunisvaara@edu.keuda.fi>
2022-04-19 15:02:34 +0300

    toggle btn 19.4

---
+++ 2303/testi.js
@@ -0,0 +1,15 @@
+var x = document.getElementById("tyo");
+var y = document.getElementById("hallinta");
+var z = document.getElementById("btn");
+
+function kirjauduHallinta() {
+  x.style.left = "-400px";
+  y.style.left = "50px";
+  z.style.left = "130px";
+}
+
+function kirjauduTyo() {
+  x.style.left = "50px";
+  y.style.left = "450px";
+  z.style.left = "130px";
+}
--- ConstructionControl/kirjaudu.js
+++ ConstructionControl/kirjaudu.js
@@ -1,14 +1,22 @@
-var x = document.getElementById("tyo");
-var y = document.getElementById("hallinta");
-var z = document.getElementById("btn");
+
 
 function kirjauduHallinta() {
-  x.style.left = "-400px";
+
+  var x = document.getElementById("tyontekijat");
+  var y = document.getElementById("hallinta");
+  var z = document.getElementById("btn");
+
+  x.style.up = "400px";
   y.style.left = "50px";
   z.style.left = "130px";
 }
 
 function kirjauduTyo() {
+
+  var x = document.getElementById("tyontekijat");
+  var y = document.getElementById("hallinta");
+  var z = document.getElementById("btn");
+
   x.style.left = "50px";
   y.style.left = "450px";
   z.style.left = "0";
