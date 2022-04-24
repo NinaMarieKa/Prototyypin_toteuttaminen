501e93d0df88e92c6bbf06d46b80c5ec1f25e232
nina.marie <nina.kaunisvaara@edu.keuda.fi>
2022-04-25 01:04:24 +0300

    välitallennus

--- ConstructionControl/kirjaudu.js
+++ ConstructionControl/kirjaudu.js
@@ -6,7 +6,7 @@ function kirjauduHallinta() {
   var y = document.getElementById("hallinta");
   var z = document.getElementById("btn");
 
-  x.style.up = "400px";
+  x.style.left = "-400px";
   y.style.left = "50px";
   z.style.left = "130px";
 }
@@ -18,6 +18,6 @@ function kirjauduTyo() {
   var z = document.getElementById("btn");
 
   x.style.left = "50px";
-  y.style.left = "450px";
+  y.style.left = "400px";
   z.style.left = "0";
 }
